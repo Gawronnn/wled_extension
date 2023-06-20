377a4e747727a3d36605208d9ecc8907d0c3b21a
Gawronnn <119967117+Gawronnn@users.noreply.github.com>
2023-06-21 00:24:05 +0200

    Added few more options

--- popup.js
+++ popup.js
@@ -1,8 +1,30 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const buttonElement = document.getElementById("myButton");
+    const imageElement = document.getElementById("powerButton");
+    const buttonElement = document.getElementById("openButton");
+    const sliderElement = document.getElementById("brightnessSlider");
 
-document.getElementById("myButton").addEventListener("click", function() {
+imageElement.addEventListener("click", function() {
     fetch('http://ledy.local/win&T=2', { method: 'GET' });
     console.log("Guzik został kliknięty!");
-});
+    });
+
+buttonElement.addEventListener("click", function() {
+    chrome.tabs.create({ url: "http://ledy.local", active: true });
+    console.log("Guzik został kliknięty!");
+    });
+
+sliderElement.addEventListener("input", function() {
+    const sliderValue = sliderElement.value;
+    const fetchURL = `http://ledy.local/win&A=${sliderValue}`;
+
+    fetch(fetchURL)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      // Process the fetched data
+    })
+    .catch(error => {
+        console.error("Error:", error);
+    });
+    });
 });
