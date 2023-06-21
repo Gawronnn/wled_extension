792f63437af401c4fd3b5c7a5584cdab951787e0
Gawronnn <119967117+Gawronnn@users.noreply.github.com>
2023-06-21 13:32:03 +0200

    Update to manifest 3

--- popup.js
+++ popup.js
@@ -2,29 +2,27 @@ document.addEventListener("DOMContentLoaded", function() {
     const imageElement = document.getElementById("powerButton");
     const buttonElement = document.getElementById("openButton");
     const sliderElement = document.getElementById("brightnessSlider");
-
-imageElement.addEventListener("click", function() {
-    fetch('http://ledy.local/win&T=2', { method: 'GET' });
-    console.log("Guzik został kliknięty!");
+  
+    imageElement.addEventListener("click", function() {
+      fetch('http://ledy.local/win&T=2', { method: 'GET' });
+      console.log("Guzik został kliknięty!");
     });
-
-buttonElement.addEventListener("click", function() {
-    chrome.tabs.create({ url: "http://ledy.local", active: true });
-    console.log("Guzik został kliknięty!");
+  
+    buttonElement.addEventListener("click", function() {
+      chrome.tabs.create({ url: "http://ledy.local", active: true });
+      console.log("Guzik został kliknięty!");
     });
-
-sliderElement.addEventListener("input", function() {
-    const sliderValue = sliderElement.value;
-    const fetchURL = `http://ledy.local/win&A=${sliderValue}`;
-
-    fetch(fetchURL)
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-      // Process the fetched data
-    })
-    .catch(error => {
-        console.error("Error:", error);
+  
+    sliderElement.addEventListener("input", function() {
+      const sliderValue = sliderElement.value;
+      const fetchURL = `http://ledy.local/win&A=${sliderValue}`;
+  
+      fetch(fetchURL)
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+          // Process the fetched data
+        });
     });
-    });
-});
+  });
+  
